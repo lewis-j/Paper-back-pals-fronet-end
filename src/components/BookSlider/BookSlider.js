7823179abb34258772e114078fb7d92339111899
Lindsey Jackson <lindseylewisjackson@gmail.com>
2022-05-12 19:51:07 -0700

    added arrows to slider

--- src/components/BookSlider/BookSlider.js
+++ src/components/BookSlider/BookSlider.js
@@ -1,19 +1,31 @@
 import React from 'react'
 import Slider from 'react-slick';
 import { UserBookCardSm } from '../UserBookCardSm';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronRight, faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+import './BookSlider.scss';
 
 const NextArrow= ({ className, style, onClick }) => {
     console.log("classname in slider arrow", className);
     
     return (
       <div
-         className='slick-next'
-        style={{ display: "block", background: "red" }}
+         className='slick-next BookSlider__arrow'
         onClick={onClick}
-      >test</div>
+      ><FontAwesomeIcon icon={faChevronRight} /></div>
     );
   }
 
+  const PrevArrow= ({ className, style, onClick }) => {
+    console.log("classname in slider arrow", className);
+    
+    return (
+      <div
+         className='slick-prev BookSlider__arrow'
+        onClick={onClick}
+      ><FontAwesomeIcon icon={ faChevronLeft} /></div>
+    );
+  }
 const BookSlider = ({books}) => {
     var settings = {
         dots: true,
@@ -21,7 +33,8 @@ const BookSlider = ({books}) => {
         speed: 500,
         slidesToShow: 4,
         slidesToScroll: 1,
-        nextArrow: <NextArrow />
+        nextArrow: <NextArrow />,
+        prevArrow:  <PrevArrow />
       };
 
    
